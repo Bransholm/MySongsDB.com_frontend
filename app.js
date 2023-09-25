19c0365887d5566adc50f2d78a7842b56ce40c4b
Bransholm <kenneth@bransholm.dk>
2023-09-26 00:40:00 +0200

    Search implemented

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict"
 
 const endpoint = "https://mysongsdbcombackend.azurewebsites.net";
-let filterValue = "artists";
+let filterValue = "artist";
+let searchValue = "";
+let contentFromSearch = [];
 
 window.addEventListener("load", start);
 
@@ -9,6 +11,7 @@ function start() {
     console.log("Hello Team10");
     getArtists()
     document.querySelector("#filter-search").addEventListener("change", filterContent);
+    document.querySelector("#search-button").addEventListener("click", searchContent);
 }
 
 async function getArtists() {
@@ -26,9 +29,9 @@ function showArtists(artists) {
   document.querySelector("#display-content").innerHTML = "";
   for (const artist of artists) {
     const html =
-        /*html*/
-      
-        `
+      /*html*/
+
+      `
             <article class="grab-artists">
             <h3>${artist.artistName}</h3>
             <p>Birthdate: ${artist.birthdate}</p>
@@ -37,7 +40,9 @@ function showArtists(artists) {
             <p>Artist ID: ${artist.artistID}</p>
             </article>
         `;
-    document.querySelector("#display-content").insertAdjacentHTML("beforeend", html);
+    document
+      .querySelector("#display-content")
+      .insertAdjacentHTML("beforeend", html);
   }
 }
 
@@ -45,15 +50,15 @@ function filterContent(event) {
     const value = event.target.value
     if (value === "artists") {
         getArtists();
-        filterValue = "artists";
+        filterValue = "artist";
         console.log(filterValue);
     } else if (value === "tracks") {
         getTracks();
-        filterValue = "tracks";
+        filterValue = "track";
         console.log(filterValue);
     } else if (value === "albums") {
         getAlbums();
-        filterValue = "albums";
+        filterValue = "album";
         console.log(filterValue);
     }
 }
@@ -120,3 +125,80 @@ function showAlbums(albums) {
   }
 }
 
+function searchContent() {
+    searchValue = document.querySelector("#search").value;
+    getSearchContent();
+ }
+
+async function getSearchContent() {
+    contentFromSearch = await fetchSearchContent();
+    showSearchContent();
+}
+
+async function fetchSearchContent() {
+    const response = await fetch(`${endpoint}/search?type=${filterValue}Name&q=${searchValue}`);
+    const data = await response.json();
+    return data;
+}
+
+function showSearchContent() {
+    if (searchValue === "") {
+        console.log("Empty search" );
+    } else if (filterValue === "artist") {
+        document.querySelector("#display-content").innerHTML = "";
+        for (const artist of contentFromSearch.artists) {
+          const html =
+            /*html*/
+            `
+            <article class="grab-artists">
+            <h3>${artist.artistName}</h3>
+            <p>Birthdate: ${artist.birthdate}</p>
+            <p>Active since: ${artist.activeSince}</p>
+            <p>Image: ${artist.artistImage}</p>
+            <p>Artist ID: ${artist.artistID}</p>
+            </article>
+        `;
+          document
+            .querySelector("#display-content")
+            .insertAdjacentHTML("beforeend", html);
+        }
+    } else if (filterValue === "track") {
+        document.querySelector("#display-content").innerHTML = "";
+        for (const track of contentFromSearch.tracks) {
+          const html =
+            /*html*/
+
+            `
+            <article class="grab-artists">
+            <h3>${track.trackName}</h3>
+            <p>length in seconds: ${track.length}</p>
+            <p>Creation year: ${track.creationYear}</p>
+            <p>Genre: ${track.genre}</p>
+            <p>Track ID: ${track.trackID}</p>
+            </article>
+        `;
+          document
+            .querySelector("#display-content")
+            .insertAdjacentHTML("beforeend", html);
+        }
+    } else {
+        document.querySelector("#display-content").innerHTML = "";
+        for (const album of contentFromSearch.albums) {
+          const html =
+            /*html*/
+
+            `
+            <article class="grab-artists">
+            <h3>${album.albumName}</h3>
+            <p>Edition: ${album.edition}</p>
+            <p>Creation year: ${album.year}</p>
+            <p>Image: ${album.albumImage}</p>
+            <p>Album ID: ${album.albumID}</p>
+            </article>
+        `;
+          document
+            .querySelector("#display-content")
+            .insertAdjacentHTML("beforeend", html);
+        }
+    }
+}
