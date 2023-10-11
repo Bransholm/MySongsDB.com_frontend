7b5ef8b9a79995a7912d2f8e7369f954a7046cae
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-10-11 15:10:11 +0200

    artist objects

---
+++ artistsClass.js
@@ -0,0 +1,13 @@
+class artist {
+  constructor(artist) {
+    this.namartistNamee = artist.artistName;
+    this.birthdate = new Date(artist.birthdate);
+    this.activeSince = artist.activeSince;
+    this.artistImage = artist.artistImage;
+    this.artistID = artist.artistID;
+  }
+  
+  // set birthdate(){
+  // }
+
+}
