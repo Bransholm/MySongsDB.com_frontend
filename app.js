f57ca8ba5bcf1c7b1b070eee15c0be964f978795
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-10-11 12:13:39 +0200

    app changes

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-"use strict"
+"use strict";
+
+import { ListRenderer } from "./listRendere.js";
+import { AritstRenderer } from "./renderHTML/renderArtist.js";
+import { TrackRenderer } from "./renderHTML/renderTrack.js";
+import { AlbumRenderer } from "./renderHTML/renderAlbum.js";
+import { showSearchContent } from "./searchFunctions.js";
+import {
+  fetchArtists,
+  fetchTracks,
+  fetchAlbums,
+  fetchSearchContent,
+} from "./fetchData.js";
 
 const endpoint = "https://mysongsdbcombackend.azurewebsites.net";
 let filterValue = "artist";
@@ -8,205 +20,82 @@ let contentFromSearch = [];
 window.addEventListener("load", start);
 
 function start() {
-    console.log("Hello Team10");
-    getArtists()
-    document.querySelector("#filter-search").addEventListener("change", filterContent);
-    document.querySelector("#search-button").addEventListener("click", searchContent);
+  console.log("Hello Team10");
+  getArtists();
+  document
+    .querySelector("#filter-search")
+    .addEventListener("change", filterContent);
+  document
+    .querySelector("#search-button")
+    .addEventListener("click", searchContent);
 }
+
+// Artist functions
 
 async function getArtists() {
-    const artists = await fetchArtists();
-    showArtists(artists);
-}
-
-async function fetchArtists() {
-    const response = await fetch(`${endpoint}/artists`);
-    const data = await response.json();
-    return data;
+  const artists = await fetchArtists();
+  console.log(artists);
+  showArtists(artists);
 }
 
 function showArtists(artists) {
-  document.querySelector("#display-content").innerHTML = "";
-  for (const artist of artists) {
-    const html =
-      /*html*/
+  const artistELement = new ListRenderer();
+  artistELement.render(artists, AritstRenderer);
+}
 
-      `
-            <article class="grab-artists">
-            <h3>${artist.artistName}</h3>
-            <p>Birthdate: ${artist.birthdate}</p>
-            <p>Active since: ${artist.activeSince}</p>
-            <p>Image: ${artist.artistImage}</p>
-            <p>Artist ID: ${artist.artistID}</p>
-            </article>
-        `;
-    document
-      .querySelector("#display-content")
-      .insertAdjacentHTML("beforeend", html);
+// Filter functionality
+
+function filterContent(event) {
+  const value = event.target.value;
+  if (value === "artists") {
+    getArtists();
+    filterValue = "artist";
+    console.log(filterValue);
+  } else if (value === "tracks") {
+    getTracks();
+    filterValue = "track";
+    console.log(filterValue);
+  } else if (value === "albums") {
+    getAlbums();
+    filterValue = "album";
+    console.log(filterValue);
   }
 }
 
-function filterContent(event) {
-    const value = event.target.value
-    if (value === "artists") {
-        getArtists();
-        filterValue = "artist";
-        console.log(filterValue);
-    } else if (value === "tracks") {
-        getTracks();
-        filterValue = "track";
-        console.log(filterValue);
-    } else if (value === "albums") {
-        getAlbums();
-        filterValue = "album";
-        console.log(filterValue);
-    }
-}
+// Track functions
 
 async function getTracks() {
   const tracks = await fetchTracks();
   showTracks(tracks);
 }
 
-async function fetchTracks() {
-  const response = await fetch(`${endpoint}/tracks`);
-  const data = await response.json();
-  return data;
-}
-
 function showTracks(tracks) {
-  document.querySelector("#display-content").innerHTML = "";
-  for (const track of tracks) {
-    const html =
-      /*html*/
-
-      `
-            <article class="grab-artists">
-            <h3>${track.trackName}</h3>
-            <p>length in seconds: ${track.length}</p>
-            <p>Creation year: ${track.creationYear}</p>
-            <p>Genre: ${track.genre}</p>
-            <p>Track ID: ${track.trackID}</p>
-            </article>
-        `;
-    document.querySelector("#display-content").insertAdjacentHTML("beforeend", html);
-  }
+  const trackELement = new ListRenderer();
+  trackELement.render(tracks, TrackRenderer);
 }
 
-////
+// Album functions
 
 async function getAlbums() {
   const albums = await fetchAlbums();
   showAlbums(albums);
 }
 
-async function fetchAlbums() {
-  const response = await fetch(`${endpoint}/albums`);
-  const data = await response.json();
-  return data;
-}
-
 function showAlbums(albums) {
-  document.querySelector("#display-content").innerHTML = "";
-  for (const album of albums) {
-    const html =
-      /*html*/
-
-      `
-            <article class="grab-artists">
-            <h3>${album.albumName}</h3>
-            <p>Edition: ${album.edition}</p>
-            <p>Creation year: ${album.year}</p>
-            <p>Image: ${album.albumImage}</p>
-            <p>Album ID: ${album.albumID}</p>
-            </article>
-        `;
-    document.querySelector("#display-content").insertAdjacentHTML("beforeend", html);
-  }
+  const albumELement = new ListRenderer();
+  albumELement.render(albums, AlbumRenderer);
 }
+
+// Search functions
 
 function searchContent() {
-    searchValue = document.querySelector("#search").value;
-    getSearchContent();
- }
+  searchValue = document.querySelector("#search").value;
+  getSearchContent();
+}
 
 async function getSearchContent() {
-    contentFromSearch = await fetchSearchContent();
-    showSearchContent();
+  contentFromSearch = await fetchSearchContent();
+  showSearchContent();
 }
 
-async function fetchSearchContent() {
-    const response = await fetch(`${endpoint}/search?type=${filterValue}Name&q=${searchValue}`);
-    const data = await response.json();
-    return data;
-}
-
-function showSearchContent() {
-    if (searchValue === "") {
-        console.log("Empty search" );
-    } else if (filterValue === "artist") {
-        document.querySelector("#display-content").innerHTML = "";
-        document.querySelector("#search").value = "";
-        for (const artist of contentFromSearch.artists) {
-          const html =
-            /*html*/
-            `
-            <article class="grab-artists">
-            <h3>${artist.artistName}</h3>
-            <p>Birthdate: ${artist.birthdate}</p>
-            <p>Active since: ${artist.activeSince}</p>
-            <p>Image: ${artist.artistImage}</p>
-            <p>Artist ID: ${artist.artistID}</p>
-            </article>
-        `;
-          document
-            .querySelector("#display-content")
-            .insertAdjacentHTML("beforeend", html);
-        }
-    } else if (filterValue === "track") {
-        document.querySelector("#display-content").innerHTML = "";
-        document.querySelector("#search").value = "";
-        for (const track of contentFromSearch.tracks) {
-          const html =
-            /*html*/
-
-            `
-            <article class="grab-artists">
-            <h3>${track.trackName}</h3>
-            <p>length in seconds: ${track.length}</p>
-            <p>Creation year: ${track.creationYear}</p>
-            <p>Genre: ${track.genre}</p>
-            <p>Track ID: ${track.trackID}</p>
-            </article>
-        `;
-          document
-            .querySelector("#display-content")
-            .insertAdjacentHTML("beforeend", html);
-        }
-    } else {
-      if (contentFromSearch.albums.length == 0) {
-        getAlbums();
-      } else {
-        document.querySelector("#display-content").innerHTML = "";
-        document.querySelector("#search").value = "";
-        console.log(contentFromSearch.albums.length);
-        for (const album of contentFromSearch.albums) {
-          const html =
-            /*html*/
-
-            `
-            <article class="grab-artists">
-            <h3>${album.albumName}</h3>
-            <p>Edition: ${album.edition}</p>
-            <p>Creation year: ${album.year}</p>
-            <p>Image: ${album.albumImage}</p>
-            <p>Album ID: ${album.albumID}</p>
-            </article>
-        `;
-          document
-            .querySelector("#display-content")
-            .insertAdjacentHTML("beforeend", html);
-        }
-      }
-    }
-}
+export { endpoint, searchValue, filterValue, contentFromSearch };
