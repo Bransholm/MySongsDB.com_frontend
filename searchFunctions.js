15a251b59de0c74184cd586f4611f988f42abf01
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-10-11 12:12:26 +0200

    search module

---
+++ searchFunctions.js
@@ -0,0 +1,38 @@
+import { ListRenderer } from "./listRendere.js";
+import { AritstRenderer } from "./renderHTML/renderArtist.js";
+import { TrackRenderer } from "./renderHTML/renderTrack.js";
+import { AlbumRenderer } from "./renderHTML/renderAlbum.js";
+import { searchValue, filterValue, contentFromSearch } from "./app.js";
+
+function clearSearchBar() {
+  document.querySelector("#search").value = "";
+}
+
+function showSearchContent() {
+  const newSearchResult = new ListRenderer();
+
+  if (searchValue === "") {
+    console.log("Empty search");
+  } else if (filterValue === "artist") {
+    document.querySelector("#display-content").innerHTML = "";
+    clearSearchBar();
+
+    newSearchResult.render(contentFromSearch.artists, AritstRenderer);
+  } else if (filterValue === "track") {
+    document.querySelector("#display-content").innerHTML = "";
+    clearSearchBar();
+
+    newSearchResult.render(contentFromSearch.tracks, TrackRenderer);
+  } else {
+    if (contentFromSearch.albums.length == 0) {
+      getAlbums();
+    } else {
+      document.querySelector("#display-content").innerHTML = "";
+      clearSearchBar();
+      console.log(contentFromSearch.albums.length);
+      newSearchResult.render(contentFromSearch.albums, AlbumRenderer);
+    }
+  }
+}
+
+export { showSearchContent };
